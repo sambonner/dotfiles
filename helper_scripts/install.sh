ec32a26f46b08fd9dbc327a4307223253983256d
Sam Bonner <sam@sambonner.co.nz>
2016-03-12 16:05:17 +1300

    Update helper script after project name change

--- helper_scripts/install.sh
+++ helper_scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd
-cp -R environment_files/.vim ~/.vim
+cp -R dotfiles/.vim ~/.vim
 sudo apt-get install exuberant-ctags x11-xserver-utils
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
 vim +PluginInstall +qall
